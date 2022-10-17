a038128a63e268b8a01316788d452bec10a8758a
Aluno <felipeleal81@gmail.com>
2022-10-17 09:19:07 -0300

    Commit Inicial

---
+++ index/index.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+index_blueprint = Blueprint("index", __name__)
+
+@index_blueprint.route("/", methods=["GET"])
+@index_blueprint.route("/index", methods=["GET"])
+def index():
+    return "Ola, Mundo! Como vai?"
